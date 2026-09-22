77fe9f9755e6306e8b0d4b96ea3893f59ba2a3b4
mkelvers <mikkelelvers@outlook.com>
2026-09-22 19:07:06 +0200

    refactor: remove api contracts

--- packages/contracts/src/contracts/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './account.js';
-export * from './auth.js';
-export * from './anime.js';
-export * from './maintenance.js';
-export * from './watchlist.js';
-export * from './notifications.js';
